ff51ae2ed6860a2a118e8d12ca021a57475fdd5a
mythridium <132613374+mythridium@users.noreply.github.com>
2023-09-04 17:32:51 +1000

    add support for game v1.2

--- src/js/setup.mjs
+++ src/js/setup.mjs
@@ -338,58 +338,7 @@ export function setup(ctx) {
     // This one allows us to inject our own HTML at a very specific time
     // when the game renders combat area selection grid.
     // We use this "injection window" to inject the "T" buttons
-    ctx.patch(CombatAreaMenu, 'createMenuElement').replace(function (fnBody, areaData, id) {    
-        const openButton = this.container.appendChild(createElement('div', {
-            classList: ['col-12', 'col-md-6', 'col-xl-4']
-        })).appendChild(createElement('div', {
-            classList: ['block', 'block-content', 'block-rounded', 'border-top', 'border-combat', 'border-4x', 'pointer-enabled', 'clickable', ],
-            id: 'clickable',
-        }));
-
-        const contentContainer = openButton.appendChild(createElement('div', {
-            classList: ['media', 'd-flex', 'align-items-center', 'push']
-        }));
-        const image = createElement('img', {
-            classList: ['shop-img'],
-            attributes: [['src', areaData.media]]
-        });
-        const infoContainer = createElement('div', {
-            classList: ['media-body']
-        });
-        contentContainer.append(createElement('div', {
-            classList: ['mr-3'],
-            children: [image]
-        }), infoContainer);
-        const unlockedElems = [];
-        const lockedElems = [];
-        const tutorialDirection = this.createTutorialDirection(areaData);
-        unlockedElems.push(this.createName(areaData));
-        let reqSpans = [];
-        if (areaData.entryRequirements.length > 0) {
-            let requirements;
-            ({reqSpans, requirements} = this.createRequirements(areaData));
-            unlockedElems.push(requirements);
-        }
-        let table;
-        let buttons;
-        const effectDescription = createElement('span');
-        if (areaData instanceof SlayerArea) {
-            unlockedElems.push(this.createEffectInfo(areaData, effectDescription));
-        }
-        if (areaData instanceof Dungeon) {
-            if (areaData.unlockRequirement !== undefined) {
-                lockedElems.push(this.createDungeonUnlock(areaData));
-            }
-            unlockedElems.push(...this.createDungeonInfo(areaData));
-            ({table, buttons} = this.createDungeonTable(areaData));
-        } else {
-            ({table, buttons} = this.createMonsterTable(areaData));
-        }
-        infoContainer.append(...lockedElems, tutorialDirection, ...unlockedElems);
-        hideElement(table);
-
-        // --------------------------------------- OVERRIDE ---------------------------------------
-
+    ctx.patch(CombatAreaMenuElement, 'setOpenOptions').after(function (fnBody, areaData) {
         let render = true;
         const skippedAreas = ['melvorTotH:Lair_of_the_Spider_Queen', 'melvorF:Into_the_Mist', 'melvorF:Impending_Darkness']
 
@@ -398,7 +347,7 @@ export function setup(ctx) {
         }
 
         if (render) {
-            const resolverAreaTargetButton = openButton.appendChild(createElement('div', {
+            const resolverAreaTargetButton = this.openButton.appendChild(createElement('div', {
                 classList: ['combat-resolver-set-area-target'],
                 text: "T"
             }))
@@ -409,77 +358,17 @@ export function setup(ctx) {
                 areaData instanceof Dungeon ? 'dungeon' : areaData instanceof SlayerArea ? 'slayer' : ''
             );
         }
-
-        // --------------------------------------- !OVERRIDE! -------------------------------------
-
-        openButton.append(table);
-        const eventButton = this.createEventStartButton(areaData);
-        hideElement(eventButton);
-        openButton.append(eventButton);
-        const menuElem = {
-            table: table,
-            image: image,
-            requirements: reqSpans,
-            fightButtons: buttons, 
-            isOpen: false,
-            lockedElems: lockedElems,
-            unlockedElems: unlockedElems,
-            isEventActive: false,
-            eventButton,
-            openButton,
-            effectDescription, 
-        };
-        openButton.onclick = ()=>this.toggleTable(areaData, menuElem);
-        this.menuElems.set(areaData, menuElem);
     });
 
+    ctx.patch(MonsterSelectTableElement, 'setArea').after(function(fnBody, areaData) {
+        const tableBody = this.tableBody;
 
-    ctx.patch(CombatAreaMenu, 'createMonsterTable').replace(function (fnBody, areaData) {    
-        const table = createElement('table', {
-            classList: ['table', 'table-sm', 'table-vcenter']
-        });
-        table.appendChild(createElement('thead')).appendChild(createElement('tr')).append(createElement('th', {
-            classList: ['text-center'],
-            attributes: [['style', 'width: 75px;']],
-            children: [createElement('small', {
-                text: '#'
-            })],
-        }), createElement('th', {
-            attributes: [['style', 'width: 125px;']],
-            children: [createElement('small', {
-                text: getLangString('COMBAT_MISC_NAME')
-            })],
-        }), createElement('th', {
-            attributes: [['style', 'width: 50px;']],
-            children: [createElement('small', {
-                text: getLangString('COMBAT_MISC_TYPE')
-            })],
-        }), createElement('th', {
-            classList: ['text-center'],
-            children: [createElement('small', {
-                text: getLangString('COMBAT_MISC_OPTIONS')
-            })],
-        }));
-        const body = table.appendChild(createElement('tbody'));
-        const buttons = [];
-        areaData.monsters.forEach((monster)=>{
-            const fightButton = createElement('button', {
-                classList: ['btn', 'btn-sm', 'btn-danger', 'm-1'],
-                attributes: [['role', 'button']],
-                text: getLangString('COMBAT_MISC_53'),
-            });
-            fightButton.onclick = ()=>game.combat.selectMonster(monster, areaData);
-            const dropsButton = createElement('button', {
-                classList: ['btn', 'btn-sm', 'btn-primary', 'm-1'],
-                attributes: [['role', 'button']],
-                text: getLangString('COMBAT_MISC_104'),
-            });
-            dropsButton.onclick = (event)=>{
-                viewMonsterDrops(monster, false);
-                event.stopPropagation();
-            };
+        tableBody.querySelectorAll('tr').forEach(function(row, index) {
+            const monster = areaData.monsters[index];
 
-            // --------------------------------------- OVERRIDE ---------------------------------------
+            if (!monster) {
+                return;
+            }
 
             const resolverMonsterTargetButton = createElement('div', {
                 classList: ['combat-resolver-set-monster-target'],
@@ -493,46 +382,10 @@ export function setup(ctx) {
                 areaData instanceof Dungeon ? 'dungeon' : areaData instanceof SlayerArea ? 'slayer' : ''
             );
 
-            // --------------------------------------- !OVERRIDE! -------------------------------------
-
-            body.appendChild(createElement('tr')).append(createElement('th', {
-                classList: ['text-center'],
-                attributes: [['scope', 'row']],
-                children: [createElement('img', {
-                    classList: ['max-height-64', 'max-width-64'],
-                    attributes: [['src', monster.media]],
-                }), ],
-            }), createElement('td', {
-                classList: ['font-w600', 'font-size-sm'],
-                text: monster.name,
-                children: [createElement('br'), createElement('small', {
-                    classList: ['font-w400'],
-                    text: templateString(getLangString('COMBAT_MISC_93'), {
-                        level: `${monster.combatLevel}`
-                    }),
-                }), createElement('br'), createElement('small', {
-                    children: [createElement('img', {
-                        classList: ['skill-icon-xs', 'mr-2'],
-                        attributes: [['src', game.hitpoints.media]],
-                    }), document.createTextNode(`${numberMultiplier * monster.levels.Hitpoints}`), ],
-                }), ],
-            }), createElement('td', {
-                classList: ['font-w600', 'font-size-sm'],
-                children: [createElement('img', {
-                    classList: ['skill-icon-xxs'],
-                    attributes: [['src', `${CDNDIR}assets/media/${CombatAreaMenu.attackTypeMedia[monster.attackType]}.svg`]],
-                }), ],
-            }), createElement('td', {
-                classList: ['text-center'],
-                children: [fightButton, dropsButton, resolverMonsterTargetButton],
-            }));
-            buttons.push(fightButton);
-        }
-        );
-        return {
-            table,
-            buttons
-        };
+            if (row.lastElementChild) {
+                row.lastElementChild.appendChild(resolverMonsterTargetButton);
+            }
+        });
     });
 
     ctx.patch(SlayerTaskMenuElement, 'updateTaskSelectButtons').replace(function (fnBody, game) {
